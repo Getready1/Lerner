fff66a8ce51e53c48c3de20c380b341b6266942d
STibulschii <Serghei.Tibulschii@endava.com>
2019-04-26 17:21:24 +0300

    =noun cards

--- Lern.Web/ClientApp/src/app/home/home.component.ts
+++ Lern.Web/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styles: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.words = [
+      {
+        text: "Leben",
+        artikel: Artikel.das
+      },
+      {
+        text: "Frau",
+        artikel: Artikel.die
+      },
+      {
+        text: "Rauch",
+        artikel: Artikel.der
+      }
+    ]
+
+    this.current = this.words[0]
+  }
+  words: Noun[]
+  current: Noun
+  counter:number = 0
+  noMore: boolean
+
+  artikelBtnClick = ($event) => {
+    const temp: Noun = this.getNext().next().value
+    if(temp) this.current = temp
+    else this.noMore = true
+  }
+
+  *getNext(){
+    yield this.words[++this.counter];
+  }
+}
+
+interface Noun {
+  text: string,
+  artikel: Artikel
+}
+
+enum Artikel {
+  der = 1,
+  die,
+  das
 }
